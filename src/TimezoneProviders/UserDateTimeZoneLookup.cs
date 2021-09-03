213cbd9f0b76fe6b863840716fca8264ed7081a9
Steven H <github@steven-h.net>
2021-09-03 13:48:01 -0400

    A little more work done

--- src/TimezoneProviders/UserDateTimeZoneLookup.cs
+++ src/TimezoneProviders/UserDateTimeZoneLookup.cs
@@ -1,11 +1,36 @@
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+
 namespace Nixill.Discord.ChiselTime.Timezones
 {
   public class UserDateTimeZoneLookup
   {
-    private static UserDateTimeZoneLookup _Instance;
-    public static UserDateTimeZoneLookup Instance
-    {
+    private const string GetCmd =
+      @"SELECT tzZone
+        FROM userZones
+        WHERE tzName = $name;";
 
+    private const string SetCmd =
+      @"INSERT OR REPLACE INTO userZones
+        (tzName, tzZone)
+        VALUES ($name, $zone);";
+
+    private const string DelCmd =
+      @"DELETE FROM userZones
+        WHERE tzName = $name;";
+
+    private static UserDateTimeZoneLookup _Instance;
+    private SqliteConnection conn;
+
+    public static async Task<UserDateTimeZoneLookup> GetInstance()
+    {
+      if (_Instance == null)
+      {
+        _Instance = new UserDateTimeZoneLookup();
+        _Instance.conn = new SqliteConnection("Data Source=cfg/zones.db");
+        await _Instance.conn.OpenAsync();
+      }
+      return _Instance;
     }
   }
 }
